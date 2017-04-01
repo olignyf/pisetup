548d3da82cd9557882270a9eeecb84ea87cb40e5
Francois Oligny-Lemieux <flemieux@haivision.com>
2017-04-01 12:30:54 -0400

    core.sh and update.sh

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+##############################
+echo "Enabling SSH"
+sudo systemctl enable ssh.service
+sudo systemctl restart ssh.service
+
+##############################
+echo "Updating APT REPOS"
+sudo apt-get update
+
+##############################
+echo "Updating system packages"
+sudo apt-get --assume-yes upgrade
+
+##############################
+echo "Installing showip LXPANEL"
+sudo apt-get --assume-yes install libfm-dev libfm-gtk-dev
+git clone https://github.com/olignyf/raspberry-pi-showip.git
+cd raspberry-pi-showip
+make
+sudo python install.py
+cd ..
+
+##############################
+echo "Installing samba server"
+pwd
+sudo apt-get --assume-yes install samba samba-common-bin
+sudo service smbd restart
+sudo service nmbd restart
+sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.ori
+sudo cp smb.conf /etc/samba/
+sudo service smbd restart
+(echo raspberry; echo raspberry) | sudo smbpasswd -a pi
+
+
+
